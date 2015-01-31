d005a026ccec6507b2bc405914977d07402daa73
Qtrain <michaelquatrani@gmail.com>
2015-01-31 15:08:20 -0500

    html and css

---
+++ Project 4 - StreetFighter/js/app.js
@@ -0,0 +1,7 @@
+/* Using .show() and .hide()
+ http://www.w3schools.com/jquery/jquery_hide_show.asp
+ */
+/* make sure document is ready i.e. page is loaded before executing jquery */
+$(document).ready(function(){
+
+});
